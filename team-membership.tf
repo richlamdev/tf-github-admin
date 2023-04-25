0b70e0e1202d4cec5039e9ebd6f492e81c52e53c
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2023-04-25 15:22:26 -0700

    apparently works for team-memberships, TBD

--- team-membership.tf
+++ team-membership.tf
@@ -1,3 +1,26 @@
+#locals {
+#  team_membership = jsondecode(file("team_memberships.json"))
+#}
+#
+#resource "github_team_membership" "team_memberships" {
+#  for_each = {
+#    for team in local.team_membership :
+#    team.id => {
+#      team_id = team.id
+#      member = {
+#        for m in team.members :
+#        m.username => {
+#          username = m.username
+#          role = m.role
+#        }
+#      }
+#    }
+#  }
+#  team_id = each.value.team_id
+#  username = each.value.members[*].username
+#  role = each.value.members[*].role
+#}
+
 locals {
   team_membership = jsondecode(file("team_memberships.json"))
 }
@@ -7,13 +30,18 @@ resource "github_team_membership" "team_memberships" {
     for team in local.team_membership :
     team.id => {
       team_id = team.id
-      member = {
+      members = [
         for m in team.members :
-        m.username => {
+        {
           username = m.username
           role = m.role
         }
-      }
+      ]
     }
   }
+
+  team_id   = each.value.team_id
+  username  = element(each.value.members[*].username, 0)
+  role     = element(each.value.members[*].role,0)
 }
+
