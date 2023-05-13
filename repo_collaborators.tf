059da7d6f1953f08862be90f7c7631c755f26324
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2023-05-13 00:14:38 -0700

    flatten repo-collaborators.json with terraform

--- repo_collaborators.tf
+++ repo_collaborators.tf
@@ -1,31 +1,19 @@
-# Define a `locals` block to flatten the JSON data
 locals {
-  flattened_data = flatten([
-    for obj in jsondecode(file("repo-collaborators.json")) : {
-      for team in obj.team : "team_${team.team[0]}_${obj.repository}" => {
-        repository = obj.repository
-        username = team.team[0]
-        permission = team.permission[0]
-      }
-      for user in obj.user : [
-        for i, username in enumerate(user.usernames) : {
-          "user_${username}_${obj.repository}" => {
-            repository = obj.repository
+  repositories = flatten([    for repo in jsondecode(file("repo-collaborators.json")) :    [      for team in repo["team:"] :
+      [
+        for user in repo["user"] :
+        [
+          for username in user["usernames"] :
+          {
+            repository = repo["repository"]
+            team = length(team["team"]) > 0 ? team["team"][0] : null
+            permission = length(team["permission"]) > 0 ? team["permission"][0] : null
             username = username
-            permission = user.permission[i]
+            user_permission = user["permission"][index(user["usernames"], username)]
           }
-        }
+        ]
       ]
-    }
+    ]
   ])
-}
-
-# Generate the Terraform resource file for GitHub repository collaborators
-resource "github_repository_collaborators" "example" {
-  for_each = local.flattened_data
-
-  repository = each.value.repository
-  username = each.value.username
-  permission = each.value.permission
 }
 
