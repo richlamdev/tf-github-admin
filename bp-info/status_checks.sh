5515e1404e2a109d5e10573ae3690db5cfd2139d
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2023-06-28 22:09:00 -0700

    minor update to status_checks.sh

--- bp-info/status_checks.sh
+++ bp-info/status_checks.sh
@@ -2,5 +2,8 @@
 
 echo "checking for required status checks"
 #grep -A 10 '"required_status_checks":' ../branch-protection/*
-grep -A2 '"checks": \[' ../branch-protection/*
+grep '"checks": \[\]' ../branch-protection/*
 
+echo
+echo "number of repos without status checks:"
+grep '"checks": \[\]' ../branch-protection/* | wc -l
