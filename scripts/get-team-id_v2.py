577d33b852a3ce4d7939b0a656820346d1415d3e
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2023-07-10 22:58:41 -0700

    temporary save

--- scripts/get-team-id_v2.py
+++ scripts/get-team-id_v2.py
@@ -58,15 +58,13 @@ def get_team_id_and_parent_slug(team_slug: str) -> dict:
     }
 
 
-def get_team_and_parent():
-    if len(sys.argv) > 1:
-        team_slug = sys.argv[1]
-        team_and_parent = get_team_id_and_parent_slug(team_slug)
-        print(
-            f"Team ID: {team_and_parent.get('id')}, Parent Team ID: {team_and_parent.get('parent_id')}, Parent Team Slug: {team_and_parent.get('parent_slug')}"
-        )
-    else:
-        print("Please provide a team slug as the first argument.")
+def get_team_by_id(team_id: str) -> dict:
+    """Get a team's detail by its ID"""
+    teams = github_api_request(f"/orgs/{org}/teams")
+    for team in teams:
+        if team.get("id") == int(team_id):
+            return team
+    return {}
 
 
 if __name__ == "__main__":
@@ -87,4 +85,20 @@ if __name__ == "__main__":
         print('Eg: export GITHUB_OWNER="google"')
         sys.exit(1)
 
-    get_team_and_parent()
+    if len(sys.argv) > 2:
+        option = sys.argv[1]
+        parameter = sys.argv[2]
+        if option == "-s":
+            team_and_parent = get_team_id_and_parent_slug(parameter)
+            print(
+                f"Team ID: {team_and_parent.get('id')}, Parent Team ID: {team_and_parent.get('parent_id')}, Parent Team Slug: {team_and_parent.get('parent_slug')}"
+            )
+        elif option == "-t":
+            team = get_team_by_id(parameter)
+            print(f"Team: {team}")
+        else:
+            print(
+                "Invalid option. Please provide -s for team slug or -t for team id."
+            )
+    else:
+        print("Please provide an option and parameter.")
